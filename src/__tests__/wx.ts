57b39e5362174cf74ca07daff5e8d450841b94c2
fragment0 <hi@fragment0.com>
2019-05-07 00:18:32 +0800

    add wx test

---
+++ src/__tests__/wx.ts
@@ -0,0 +1,94 @@
+import {string2any} from '../base'
+
+const baseResponse = {
+  statusCode: 200,
+  header: {},
+  data: '{}',
+}
+let response: any = {...baseResponse}
+
+const mockedFetch = jest.fn((options: any) => {
+  options.success(response)
+})
+let http: any
+
+beforeAll(async () => {
+  (global as any).wx = {
+    request: mockedFetch,
+  }
+  http = (await import('../index')).http
+})
+
+afterAll(() => {
+  delete (global as any).wx
+})
+
+test('test basic function', async () => {
+  expect.assertions(2)
+
+  try {
+    const res = await http.get('/test')
+    expect(res.status).toBe(200)
+    expect(res.data).toBe('{}')
+  } catch (e) {
+    throw e
+  }
+})
+
+test('test json format', async () => {
+  expect.assertions(1)
+
+  response = {
+    ...baseResponse,
+    header: {
+      'content-length': '10',
+      'content-type': 'application/json',
+    },
+    data: {test: 1},
+  }
+
+  const res = await http.get('/test')
+  expect((res.data as string2any).test).toBe(1)
+})
+
+test('test failed', async () => {
+  expect.assertions(1)
+
+  response = {
+    ...baseResponse,
+    statusCode: 404,
+  }
+
+  try {
+    const res = await http.get('/test')
+  } catch (e) {
+    if (!e.data) throw e
+    expect(e.status).toBe(404)
+  }
+})
+
+test('test query', () => {
+  response = {
+    ...baseResponse,
+  }
+
+  http.get('/test', {
+    query: {
+      c: 2,
+    },
+  })
+
+  expect(mockedFetch.mock.calls[mockedFetch.mock.calls.length - 1][0].url).toContain('c=2')
+})
+
+test('test post body', () => {
+  http.post('/test', {
+    test: 1,
+  })
+
+  function getPostArg() {
+    return mockedFetch.mock.calls[mockedFetch.mock.calls.length - 1][0]
+  }
+  expect(getPostArg().data.test).toBe(1)
+  // expect(getPostArg().header['content-type']).toBe('application/json')
+})
